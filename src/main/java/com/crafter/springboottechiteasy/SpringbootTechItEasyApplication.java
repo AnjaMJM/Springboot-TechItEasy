63451fa8eb595bfc251d8bf9a4e5ad3238843cc2
AnjaMJM <anja.molenaar@gmail.com>
2024-09-04 13:29:03 +0200

    first commit

---
+++ src/main/java/com/crafter/springboottechiteasy/SpringbootTechItEasyApplication.java
@@ -0,0 +1,13 @@
+package com.crafter.springboottechiteasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTechItEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTechItEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crafter/springboottechiteasy/SpringbootTechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crafter.springboottechiteasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTechItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
